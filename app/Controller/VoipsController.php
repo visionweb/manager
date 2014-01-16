00fdb175e3d74357ce42295936978a860a01c041
root <root@poligon-laptop.(none)>
2014-01-16 23:46:31 +0200

    Added admin_listAccount to VoipsController

--- app/Controller/VoipsController.php
+++ app/Controller/VoipsController.php
@@ -43,7 +43,6 @@ class VoipsController extends AppController {
      * @return void
      */
     public function admin_add() {
-
     }
 
     /**
@@ -77,14 +76,43 @@ class VoipsController extends AppController {
 
     }
 
-    public function admin_listAccount(){
-        exec("curl --digest --insecure 'https://178.33.172.70:50051/1.1/users'", $dataBrut);
-        $this->set("listUser", $dataBrut);
-        //debug();
+    
+	
+	public function admin_listAccount(){
+		exec("curl --digest --insecure -u managero:UBIBOzULRSuh 'https://178.33.172.71:50051/1.0/users/'", $dataBrut);
+		exec("curl --digest --insecure -u managero:UBIBOzULRSuh 'https://178.33.172.71:50051/1.1/user_links'", $links);
+		exec("curl --digest --insecure -u managero:UBIBOzULRSuh 'https://178.33.172.71:50051/1.1/lines_sip'", $sip);
+		$sip=json_decode($sip[0], true);
+		$links=json_decode($links[0], true);
+		$dataBrut=json_decode($dataBrut[0], true);
+		$dataBrut=$dataBrut["items"];
+		$links=$links["items"];
+		$sip=$sip["items"];
+        
+		for ($i=0; $i<count($dataBrut); $i++){
+			foreach($links as $link){
+				foreach($sip as $sip_l){
+					if ($sip_l["id"]==$link["line_id"] and $dataBrut[$i]["id"]==$link["user_id"]  ){
+						$newName=$sip_l["username"];
+						$dataBrut[$i]["username"]=$newName;
+						break;
+						}
+					}
+				if (empty($dataBrut[$i]["username"])==0) break;
+				}
+			}
+		$this->set("listUser", $dataBrut);
+		$this->set("title", "Liste compte");
+        //debug();curl --digest --insecure -u managero:UBIBOzULRSuh https://178.33.172.71:50051/1.0/users/
     }
 
-    public function admin_newAccount(){
 
+    public function admin_newAccount(){
+		//If there is data send by a form
+        if ($this->request->is('post')) {
+			$this->redirect(array('action' => 'admin_listAccount'));
+			}
+			
     }
 
     public function admin_consommation(){
